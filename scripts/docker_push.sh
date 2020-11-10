973e87203abd363cf263316262fb0f08ec6e4262
George McIntosh <george@georgemcintosh.com>
2020-11-10 15:51:59 +0000

    Add latest tag

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -8,5 +8,5 @@ export VERSIONTAG=$TRAVIS_BUILD_ID"-"$BRANCH
 
 echo "Building with tags [$VERSIONTAG]"
 
-docker build -t eu.gcr.io/census-int-ci/website-generator:$VERSIONTAG -f Dockerfile.generator .
+docker build -t eu.gcr.io/census-int-ci/website-generator:$VERSIONTAG  -t eu.gcr.io/census-int-ci/website-generator:latest -f Dockerfile.generator .
 docker push eu.gcr.io/census-int-ci/website-generator:$VERSIONTAG
