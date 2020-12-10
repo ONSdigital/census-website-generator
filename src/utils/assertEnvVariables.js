bdf0c162a42be66b352485a2daec2127de027501
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2020-12-10 14:23:34 +0000

    Allow empty environment variables in assertion.

--- src/utils/assertEnvVariables.js
+++ src/utils/assertEnvVariables.js
@@ -1,6 +1,6 @@
 export default function assertEnvVariables(variableNames) {
   for (let key of variableNames) {
-    if (!process.env[key]) {
+    if (process.env[key] === undefined) {
       throw new Error(`Missing environment variable '${key}'!`);
     }
   }
