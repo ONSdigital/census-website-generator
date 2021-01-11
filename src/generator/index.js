a8c88d3ada6ea054af678f2772d446a5d32cb5e8
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2021-01-11 13:13:57 +0000

    Remove leading slash from RH URL remapping.

--- src/generator/index.js
+++ src/generator/index.js
@@ -100,7 +100,7 @@ function getLocalizedUrls(entry, sitesSourceData) {
       const htmlFixer = createStringReplacer({
         [sourceData.craftBaseUrl]: sourceData.siteBaseUrl,
         [process.env.ONS_GOOGLE_CLOUD_BUCKET_URL]: sourceData.siteBaseUrl,
-        "$SITE_BASE_PATH$": `/${sourceData.rhBaseUrl}`,
+        "$SITE_BASE_PATH$": `${sourceData.rhBaseUrl}`,
         "<table>": '<table class="table table--scrollable">',
         "<thead>": '<thead class="table__head">',
         "<tbody>": '<tbody class="table__body">',
